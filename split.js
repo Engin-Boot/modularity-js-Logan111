bd8e22c7c736116068a1ad56df0408eab8817662
BUNARI PRANAY <36372708+Logan111@users.noreply.github.com>
2020-08-24 23:40:53 +0530

    Create split.js

---
+++ split.js
@@ -0,0 +1,24 @@
+
+const expect = require('chai').expect;
+const indexref = require("./index")
+
+
+
+function testNumberToPair(number, expectedMajor, expectedMinor) {
+    const pairOfColors = indexref.GetColorFromPairNumber(number);
+    console.log(`${number} = ${expectedMajor} ${expectedMinor}`);
+    expect(pairOfColors.major).equals(expectedMajor);
+    expect(pairOfColors.minor).equals(expectedMinor);
+}
+
+function testColorToNumber(majorColor, minorColor, expectedNumber) {
+    const pairNumber = indexref.GetPairNumberFromColors(majorColor, minorColor);
+    console.log(`${majorColor} ${minorColor} = ${pairNumber}`);
+    expect(pairNumber).to.equal(expectedNumber);
+}
+
+console.log("color coder");
+testNumberToPair(4, "white", "brown");
+testNumberToPair(25, "violet", "slate");
+testColorToNumber("black", "brown", 14);
+testColorToNumber("yellow", "green", 18);
